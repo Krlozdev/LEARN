10af30690ee2595d3cca992e7de7479c181df3c8
krlozdev <krlozexile@gmail.com>
2025-02-13 15:52:48 -0500

    actualizacion de ejercicio

--- Ejercicios.py
+++ Ejercicios.py
@@ -1,10 +1,39 @@
-nombre = "Juan Carlos"
-edad = "34"
-pais = "peru"
+print("****** Sistema de reserva de hoteles ******")
 
-print("nombre : ", nombre)
+user = input("ingrese su nombre ")
 
-print("edad : ", edad)
+days_to_stay = int(input("ingrese los dias de estancia "))
 
-print("pais : ", pais)
+type_of_room = input("que tipo de habitacion desea(grande/mediana/pequeña) ")
+
+window_view = input("¿Quiere su habitacion con vista al mar? ")
+
+
+cost_of_room = 0
+
+if type_of_room == "grande":
+
+    cost_of_room = 1000
+
+elif type_of_room == "mediana":
+
+    cost_of_room = 500
+
+elif type_of_room == "pequeña":
+
+    cost_of_room = 250
+
+print("****** Sistema de reserva de hoteles ******")
+
+print("cliente: ",user)
+
+print("dias de estancia: ",days_to_stay)
+
+print("tarifa diaria: ",cost_of_room)
+
+print("¿Habitacion con vista al mar? ",window_view)
+
+
+    
+
 
